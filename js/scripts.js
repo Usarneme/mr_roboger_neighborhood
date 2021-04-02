968a27b91638f505aebdad82e3a2c45aa81389a6
Usarneme <jamestcoop@gmail.com>
2021-04-02 09:27:40 -0700

    simplifies processing logic by keeping the number input as a string. uses a continue statement to leave the loop when it finds a match for input===1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 function beepBoop(number) {
   // Confirm the number is a number and not alphanum or something else
-  const integer = Number(number)
-  if (!Number.isNaN(integer)) return new Error("You must submit a number. Please try again.")
-  if (integer === 0) return [0]
+  if (Number.isNaN(Number(number)) && number !== "0") return new Error("You must submit a number. Please try again.")
+  if (number === "0") return [0]
+  const outputArray = [0]
 
-  const outputArray = []
-  for (let index = 0; index < integer; index += 1) {
+  for (let index = 1; index <= Number(number); index += 1) {
+    console.log("looping",number,index)
+    const numberArray = number.split("")
+    console.log("numberArray",numberArray)
+    if (index === 1 || numberArray.includes("1")) {
+      outputArray.push("Beep!")
+      continue
+    }
     outputArray.push(index)
   }
+  console.log(outputArray)
+  return outputArray
 }
