e103cc52a65b19e1585501a5fda34206c38c1a76
Usarneme <jamestcoop@gmail.com>
2021-04-02 13:27:38 -0700

    separates beepBoop into a separate file which is exported, then imported into js/scripts.js using a type=module tag in the html script tag.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,4 @@
-function beepBoop(number) {
-  // Confirm the number is a number and not alphanum or something else
-  if (Number.isNaN(Number(number)) && number !== "0") return new Error("You must submit a number. Please try again.")
-  if (number === "0") return [0]
-  const outputArray = [0]
-
-  for (let index = 1; index <= Number(number); index += 1) {
-    const numbers = (""+index).split("")
-    if (index === 3 || numbers.includes("3")) {
-      outputArray.push("Won't your be my neighbor?")
-      continue
-    }
-    if (index === 2 || numbers.includes("2")) {
-      outputArray.push("Boop!")
-      continue
-    }
-    if (index === 1 || numbers.includes("1")) {
-      outputArray.push("Beep!")
-      continue
-    }
-    outputArray.push(index)
-  }
-  return outputArray
-}
+import { beepBoop } from './beepBoop.js'
 
 function resetApplication() {
   $(".results").text("").hide()
@@ -61,3 +38,4 @@ $(document).ready(function() {
 
   $("#resetButton").click(resetApplication)
 })
+
