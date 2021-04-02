1197cefe3a6ba23da9a0713295805078e8e03272
Usarneme <jamestcoop@gmail.com>
2021-04-02 13:37:50 -0700

    adds a test-watch script to package.json. adds a third passing test to scripts.spec.js

--- __tests__/scripts.spec.js
+++ __tests__/scripts.spec.js
@@ -11,3 +11,12 @@ test("It should return an error if a non-number string is input", () => {
   expect(val).toEqual(new Error("You must submit a number. Please try again."))
 })
 
+test("It should return an error if a non-number alphanumeric string is input", () => {
+  const val = beepBoop("123abc")
+  expect(val).toEqual(new Error("You must submit a number. Please try again."))
+})
+
+test("It should return an array of [0] when passed input of '0'", () => {
+  const val = beepBoop("0")
+  expect(val).toEqual([0])
+})
