9ece73965d642c6312737f83fc2e2f8bc5d70906
Usarneme <jamestcoop@gmail.com>
2021-04-02 13:33:30 -0700

    adds a first passing test to jest test suite (after the sanity check test)

--- __tests__/scripts.spec.js
+++ __tests__/scripts.spec.js
@@ -1,44 +1,13 @@
 const { test, expect } = require("@jest/globals");
 const { describe } = require("yargs");
+import { beepBoop } from "../js/beepBoop"
 
 test('sanity check', () => {
   expect(true).toEqual(true)
 })
-// test("It should return an error if a non-number string is input", () => {
-//   expect(beepBoop("howdy partner")).toBe("Error(\"You must submit a number. Please try again.\")")
-// })
 
-// describe('beepBoop func', () => {
-//   test('sanity 2', () => {
-//     expect(true).toEqual(true)
-//   })
-// })
+test("It should return an error if a non-number string is input", () => {
+  const val = beepBoop("howdy partner")
+  expect(val).toEqual(new Error("You must submit a number. Please try again."))
+})
 
-describe('outer', () => {
-  console.log('describe outer-a');
-
-  describe('describe inner 1', () => {
-    console.log('describe inner 1');
-    test('test 1', () => {
-      console.log('test for describe inner 1');
-      expect(true).toEqual(true);
-    });
-  });
-
-  console.log('describe outer-b');
-
-  test('test 1', () => {
-    console.log('test for describe outer');
-    expect(true).toEqual(true);
-  });
-
-  describe('describe inner 2', () => {
-    console.log('describe inner 2');
-    test('test for describe inner 2', () => {
-      console.log('test for describe inner 2');
-      expect(false).toEqual(false);
-    });
-  });
-
-  console.log('describe outer-c');
-});
